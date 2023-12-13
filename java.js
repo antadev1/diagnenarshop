c26a7f98ae0999548cde8bdce3e3a68067706ebb
antadev1 <fatouanta.diagne@gmail.com>
2023-12-13 17:08:23 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,39 @@
+let btndecouvrir = document.getElementById("btn1");
+console.log(btndecouvrir);
+btndecouvrir.addEventListener('click', decouvrir);
+
+
+function decouvrir(){
+    window.location.href = "nouveaute.html";
+}
+// acceuil.js
+
+let btnenvoyer = document.getElementById("btn");
+btnenvoye.addEventListener('click', envoyer);
+function envoye(){
+    alert("MESSAGE ENVOYER !");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
